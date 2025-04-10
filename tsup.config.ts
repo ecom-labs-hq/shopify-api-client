6977210ce7dd1af2c1b2af5857052dbebf3e3d30
Hexatare <120329437+Hexatare@users.noreply.github.com>
2025-04-10 20:47:42 +0200

    feat: building and exporting all four api versions

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/2025-01/index.ts", "src/2025-04/index.ts"],
+    entry: [
+        "src/2024-07/index.ts",
+        "src/2024-10/index.ts",
+        "src/2025-01/index.ts",
+        "src/2025-04/index.ts",
+    ],
     format: ["cjs", "esm"],
     platform: "neutral",
 
