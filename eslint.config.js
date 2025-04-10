1ef9ded4795b2f89cca1beb0ea02970d211590fe
Hexatare <120329437+Hexatare@users.noreply.github.com>
2025-04-10 18:21:50 +0200

    Installed eslint and added lint script

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    /* Basic config */
+    {
+        files: ["**/*.ts"],
+    },
+    {
+        ignores: ["**/dist/"],
+    },
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+    },
+
+    /* Recommended plugins */
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    /* Disable stylistic settings */
+    eslintConfigPrettier,
+];
